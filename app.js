3664a038bb2a32c437b04f9ceb01e331801f00f3
benrosenthal <benrosenthal.programming@gmail.com>
2017-02-04 22:18:50 -0500

    First Commit -- Prior to any routes set up.

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require("express");
+var app = express();
+
+
+//first peice of middleware
+app.use(function(req, res, next){
+  console.log("First piece of middleware");
+  next();
+})
+
+app.use(function(req, res, next){
+  console.log("Second piece of middleware");
+  next();
+})
+
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log("Express server is listening on port ", port);
+});
