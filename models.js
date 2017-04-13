9eb04df927879869f3fa5fcaa11199efdba2391c
benrosenthal <benrosenthal.programming@gmail.com>
2017-04-13 06:38:12 -0400

    Add files via upload

--- models.js
+++ models.js
@@ -4,6 +4,17 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema 
 
+var sortAnswers = function(a, b) {
+	//- negative a before b
+	//0 no chnage
+	//+ positive a after n
+	if(a.votes === b.votes){
+		return b.updatedAt - a.updatedAt;
+	}
+	return b.votes - a.votes;
+}
+
+
 
 var AnswerSchema = new Schema({
 	text: String,
@@ -12,10 +23,36 @@ var AnswerSchema = new Schema({
 	votes: {type: Number, default: 0}
 });
 
+//apply updates to an answer document and we also want the answers update to be current time
+//Instance mthoed becuase we can it directly on the onject when we want to update it 
+//AnswerSchema.methods.update = function(updates, callback) {}
+//Line 28 can also be written like this
+AnswerSchema.method("update", function(updates, callback) {
+	Object.assign(this, updates, {updatedAt: new Date()});
+	//now need to save parent doc -- in other words, the question associated with the answer
+	//to access question, can use parent method
+	this.parent().save(callback);
+});
+
+//Vote Instance Method -- To help with translating strings from the URL to numbers that move the count up of r downm
+AnswerSchema.method("vote", function(vote, callback) {
+	if(vote === "up") {
+		this.votes += 1;
+	} else {
+		this.votes -= 1;
+	}
+	this.parent().save(callback);
+});
+
 var QuestionSchema = new Schema({
 	text: String, 
 	createdAt: {type: Date, default: Date.now}, 
 	answers:[AnswerSchema]
+});
+
+QuestionSchema.pre("save", function(next){
+	this.answers.sort(sortAnswers); //pass in sort founction int the sort method
+	next();
 });
 
 //With two schemas that are connected together, we can create and export a question model
--- routes.js
+++ routes.js
@@ -2,12 +2,28 @@
 
 var express = require("express");
 var router = express.Router();
+var Question = require("./models").Question;
 
 // GET /listofconcerts ==> LIKE GET LIST OF CONCERTS
 //route for list of concerts in a given cit
-router.get("/", function(req, res){
-  res.json({response: "You sent me a GET request"});
+// router.get("/", function(req, res){
+//   Question.find({}, null, {sort: {createdAt: -1}} function(err, questions) {
+//       if(err) return next(err);
+//       res.json(questions);
+//   });
+//   res.json({response: "You sent me a GET request"});
+// });
+
+// "/" the above route can also be written this way:
+router.get("/", function(req, res, next) {
+    Question.find({})
+                .sort({createdAt: -1})
+                .exec(function(err, questions) {
+                  if(err return next(err);
+                  res.json(questions);
+                });
 });
+
 
 // POST /listofconcerts/city
 router.post("/", function (req, res) {
